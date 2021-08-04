cf249caf44b6b980cf39e85d6e9e2006cf580104
Harry Albietz <harry.albietz@t-online.de>
2021-08-04 13:11:56 +0200

    Methode DeleteBox und Methode ChangeBox erstellt und getestet.

--- CSHP04D_Einsendeaufgabe/Program.cs
+++ CSHP04D_Einsendeaufgabe/Program.cs
@@ -39,6 +39,7 @@ namespace CSHP04D_Einsendeaufgabe
                 //Usereingabe verarbeiten:
                 while(selection !=6)
                 {
+                    Console.Clear();
                     Console.WriteLine("Hauptmenü");
                     Console.WriteLine("-------------------------------------------");
                     Console.WriteLine("\t[1] add a new box");
@@ -50,7 +51,6 @@ namespace CSHP04D_Einsendeaufgabe
                     Console.Write("Your selection: ");
                     selection = Convert.ToInt32(Console.ReadLine());
 
-                    //Console.Clear();
                     
 
                     switch (selection)
@@ -61,9 +61,11 @@ namespace CSHP04D_Einsendeaufgabe
                             break;
                         case 2:
                             //Methode Delete
+                            DeleteBox(lager);
                             break;
                         case 3:
-                            //Methode Change
+                            //Methode ChangeBox
+                            ChangeBox(lager);
                             break;
                         case 4:
                             //Methode ShowBox
@@ -79,16 +81,113 @@ namespace CSHP04D_Einsendeaufgabe
             
             
         }
+        //Die Methode zum Ändern der Daten einer Box
+        static void ChangeBox(Box[] abox)
+        {
+            //User informieren:
+            Console.WriteLine();
+            Console.WriteLine("----------Boxdaten ändern----------");
+            //Methodenvariablen:
+            int selectedID;
+            int index;
+            Console.Write("Bitte geben Sie eine Box-Nummer zwischen 1 und 75 ein: ");
+            selectedID = Convert.ToInt32(Console.ReadLine());
+
+            //Arraybereich überwachen:
+            if(selectedID >=1 && selectedID <= 75)
+            {
+                index = selectedID - 1;
+                if (abox[index].isActiv == true)
+                {
+                    Console.Write("Bitte geben Sie die neue Länge ein: ");
+                    abox[index].Laenge = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Bitte geben Sie die neue Breite ein: ");
+                    abox[index].Breite = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Bitte geben Sie die neue Höhe ein: ");
+                    abox[index].Hoehe = Convert.ToInt32(Console.ReadLine());
+                    //Volumen neu berechnen:
+                    abox[index].Volumen = abox[index].Laenge * abox[index].Breite * abox[index].Hoehe;
+                    Console.WriteLine();
+                    Console.WriteLine($"Die Daten für die Box-ID: {selectedID} wurden erfolgreich geändert.");
+                    Console.WriteLine();
+                    Console.WriteLine("Beliebige Taste drücken um fortzufahren...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Die Box mit der ID: {selectedID} existiert nicht, bitte neu anlegen!");
+                    Console.WriteLine();
+                    Console.WriteLine("Beliebige Taste drücken um fortzufahren...");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Die von Ihnen eingegebene Nummer: {selectedID} befindetet sich ausserhalb des gültigen Bereiches!");
+                Console.WriteLine();
+                Console.WriteLine("Beliebige Taste drücken um fortzufahren...");
+                Console.ReadKey();
+            }
+        }
+        //Die Methode zum Löschen einer Box
+        static void DeleteBox(Box[] abox)
+        {
+            //User informieren:
+            Console.WriteLine();
+            Console.WriteLine("----------Eine Kiste löschen----------");
+            //Methodenvariablen:
+            int selectedID;
+            int index;
+            Console.Write("Bitte geben Sie eine Box-Nummer zwischen 1 und 75 ein: ");
+            selectedID = Convert.ToInt32(Console.ReadLine());
+
+            //Arraybereich überwachen:
+            if(selectedID >=1 && selectedID <= 75)
+            {
+                index = selectedID - 1;
+                if (abox[index].isActiv == true)
+                {
+                    abox[index].Laenge = 0;
+                    abox[index].Breite = 0;
+                    abox[index].Hoehe = 0;
+                    abox[index].Volumen = 0;
+                    abox[index].isActiv = false;
+                    Console.WriteLine();
+                    Console.WriteLine($"Die Box mit der ID: {selectedID} wurde erfolgreich gelöscht.");
+                    Console.WriteLine();
+                    Console.WriteLine("Beliebige Taste drücken um fortzufahren...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine($"Die Box mit der ID:{selectedID} wurde bereits gelöscht");
+                    Console.WriteLine();
+                    Console.WriteLine("Beliebige Taste drücken um fortzufahren...");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Die von Ihnen eingegebene Nummer: {selectedID} befindetet sich ausserhalb des gültigen Bereiches!");
+                Console.WriteLine();
+                Console.WriteLine("Beliebige Taste drücken um fortzufahren...");
+                Console.ReadKey();
+            }
+        }
 
         //die Methode zum Einlesen einer neuen Kiste
         static void AddBox(Box[] abox)
         {
             //User informieren:
+            Console.WriteLine();
             Console.WriteLine("---------Eine neue Kiste anlegen--------");
+            Console.WriteLine();
             int selectedID;
             int index;
             Console.Write("Bitte geben Sie eine Box-Nummer zwischen 1 und 75 ein: ");
             selectedID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
             //Arraybereich überwachen:
             if(selectedID >=1 && selectedID <= 75)
@@ -108,6 +207,7 @@ namespace CSHP04D_Einsendeaufgabe
                     abox[index].isActiv = true;
 
                     //Ergebnis ausgeben:
+                    Console.WriteLine();
                     Console.WriteLine("------------------------------");
                     Console.WriteLine("Folgende Daten wurden erfasst:");
                     Console.WriteLine($"Länge:   {abox[index].Laenge}");
@@ -117,15 +217,24 @@ namespace CSHP04D_Einsendeaufgabe
                     Console.WriteLine($"Aktiv:   {abox[index].isActiv}");
                     Console.WriteLine("------------------------------");
                     Console.WriteLine($"Die Daten für die Box-ID: {selectedID} wurde erfolgreich gespeichert.");
+                    Console.WriteLine();
+                    Console.WriteLine("Beliebige Taste drücken um fortzufahren...");
+                    Console.ReadKey();
                 }
                 else
                 {
                     Console.WriteLine($"Die von Ihnen eingegebne Box-ID: {selectedID} ist bereits vergeben!");
+                    Console.WriteLine();
+                    Console.WriteLine("Beliebige Taste drücken um fortzufahren...");
+                    Console.ReadKey();
                 }
             }
             else
             {
                 Console.WriteLine("Die von Ihnen eingegebene Nummer befindetet sich ausserhalb des gültigen Bereiches!");
+                Console.WriteLine();
+                Console.WriteLine("Beliebige Taste drücken um fortzufahren...");
+                Console.ReadKey();
             }
 
         }
